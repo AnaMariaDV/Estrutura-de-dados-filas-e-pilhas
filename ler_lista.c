70863d53acc23097952c2c6248321e6351bb080b
Ana Maria <aninhatexas18@gmail.com>
2025-04-26 22:18:20 -0300

    função lerLista modificada, agora ela inverteu a ordem de leitura, para manter a ordem original da lista lida

--- ler_lista.c
+++ ler_lista.c
@@ -1,22 +1,30 @@
-#include"lib.h"
+// ler_lista.c
+#include "lib.h"
+
 void lerLista(Node **cabeca, const char *nomeArquivo) {
     clock_t inicio = clock();
     FILE *arq = fopen(nomeArquivo, "r");
-    if(arq == NULL) {
-        printf("\nErro ao abrir arquivo para leitura.\n");
+    if (arq == NULL) 
+    {
+        printf("\nErro ao abrir '%s' para leitura.\n", nomeArquivo);
         return;
     }
+
     char linha[100], nome[50];
     int rg;
-    while(fgets(linha, sizeof(linha), arq)) {
-        if(sscanf(linha, "%[^,],%d", nome, &rg) == 2) {
-            Node *novo = criarNo(nome, rg);
-            novo->next = *cabeca;
-            *cabeca = novo;
+    while (fgets(linha, sizeof(linha), arq)) 
+    {
+        if (sscanf(linha, "%49[^,],%d", nome, &rg) == 2) {
+            inserirNoFim(cabeca, nome, rg);
         }
     }
     fclose(arq);
+
     clock_t fim = clock();
     double tempo = (double)(fim - inicio) / CLOCKS_PER_SEC;
-    printf("\nLista lida do arquivo %s.\n\nTempo: %.6f s", nomeArquivo, tempo);
+    printf(
+      "\nLista carregada de '%s' na ordem original.\n"
+      "Tempo de leitura: %.6f s\n",
+      nomeArquivo, tempo
+    );
 }
